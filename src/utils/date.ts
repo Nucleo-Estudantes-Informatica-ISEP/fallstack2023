b9f206b32c51f4ccc22d59b92e8756b5d5b8c9ff
André Barros <1211299@isep.ipp.pt>
2023-11-10 22:20:03 +0000

    fix: format date function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,13 @@
 export const formatDateDDStrMonthHourMin = (date: string): string => {
   const dateObj = new Date(date);
   const month = dateObj.toLocaleString("default", { month: "numeric" });
+  const day = dateObj.getDate();
   const hour = dateObj.getHours();
   const min = dateObj.getMinutes();
-  return `${dateObj.getDate()}/${month} ${hour}:${min} `;
+
+  const formattedDay = day < 10 ? "0" + day : day;
+  const formattedHour = hour < 10 ? "0" + hour : hour;
+  const formattedMin = min < 10 ? "0" + min : min;
+
+  return `${formattedDay}/${month} ${formattedHour}:${formattedMin}`;
 };
