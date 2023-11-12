16a9acbe59e41cc9f741c8b6eb6a37bc7845350d
André L Reis <gato5andre26@gmail.com>
2023-11-12 17:42:51 +0000

    feat: finished the design of the modal

--- src/components/QRCodeModal/index.tsx
+++ src/components/QRCodeModal/index.tsx
@@ -3,7 +3,7 @@ import Image, { StaticImageData } from "next/image";
 
 import { useDisableBodyScroll } from "../../hooks/disableBackgroundMoving";
 
-import { X } from "react-bootstrap-icons";
+import { BsFillClipboardFill, BsQrCodeScan } from "react-icons/bs";
 
 interface QRCodeModalProps {
   hidden: boolean;
@@ -21,23 +21,61 @@ const QRCodeModal: React.FC<QRCodeModalProps> = ({ hidden, setHidden }) => {
 
   return !hidden ? (
     <div
-      className="scrollbar-hide fixed inset-5 z-10 overflow-y-scroll"
+      className="scrollbar-hide fixed inset-6 start-2 z-10 overflow-y-hidden rounded-lg sm:inset-4 sm:end-6 sm:start-6 md:inset-14 md:end-12 md:start-8 lg:inset-16 lg:end-14 lg:start-10"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
       onClick={() => setHidden(true)}
     >
-      <div className="fixed inset-5 bg-gray-500 bg-opacity-0 transition-opacity"></div>
+      <div className="fixed bg-gray-500 bg-opacity-0 transition-opacity"></div>
 
-      <div className="flex min-h-screen items-center justify-center">
-        <div className="max-w-7/8 relative w-full">
+      <div className="flex min-h-screen  items-center justify-center">
+        <div className="max-w-7/8 relative w-full ">
           <div
-            className="relative h-screen w-full transform  bg-white text-left shadow-xl transition-all"
+            className="relative h-screen w-full transform bg-white text-left shadow-xl transition-all"
             onClick={handleModalClick}
           >
-            <div className="grid grid-cols-1 text-black lg:grid-cols-3">
-              <div className="lg:col-span-2">01</div>
-              <div className="lg:col-span-1">02</div>
+            <div className="flex min-h-screen items-start justify-center">
+              <div className="max-w-7/8 w-full">
+                <div className="relative h-screen p-10 text-center shadow-xl sm:p-0 md:p-0 lg:p-6">
+                  <h1 className="mb-6 text-3xl font-bold text-black sm:mb-0 sm:mt-6 sm:text-3xl md:text-4xl lg:text-6xl">
+                    Partilha o teu <span className="text-primary">Perfil</span>
+                  </h1>
+                  {/* Grid */}
+                  <div className="mt-10 grid grid-cols-1 sm:mt-0 sm:grid-cols-2 md:mt-6 lg:mt-20">
+                    {/* left column */}
+                    <div className="flex items-center justify-center ">
+                      {/* THIS GONNA BE ADJUSTED WHEN THE QR CODE IS REALLY IMPLEMENTED FOR NOW ITS JUST AN ICON*/}
+                      <BsQrCodeScan
+                        style={{ fontSize: "15rem", color: "#000" }}
+                      />
+                    </div>
+                    {/* right column */}
+                    <div className="flex items-center justify-center pb-0 pt-14 sm:pb-0 sm:pt-0">
+                      <div className="flex flex-col items-center">
+                        <div className="rounded-lg bg-gray-200 p-2 sm:p-2 md:p-2">
+                          <div className="flex items-center">
+                            <input
+                              type="text"
+                              value="CODE"
+                              className="w-56 border-none bg-gray-200 p-2 text-center text-2xl font-bold text-black focus:outline-none sm:w-56 sm:text-3xl md:w-72 "
+                              disabled
+                            />
+                            <BsFillClipboardFill
+                              style={{ fontSize: "1.5rem", color: "#718096" }}
+                              className="cursor-pointer"
+                            />
+                          </div>
+                        </div>
+                        <p className="lg-mr-0 mt-6 text-sm md:text-sm lg:text-base text-black sm:mr-6 md:mr-4">
+                          Partilha o teu <b>código</b> com as empresas de forma
+                          a poderem <b>guardar</b> o teu perfil!
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
