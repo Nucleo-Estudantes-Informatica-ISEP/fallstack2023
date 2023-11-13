6ecf6c355cadc3c4e55abe1f3d7b90895fc6c344
André Barros <1211299@isep.ipp.pt>
2023-11-13 17:24:32 +0000

    fix: last edition url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const App: FunctionComponent = () => {
           <Content contentRef={contentRef} />
           <ComingSoon />
           <Footer
-            lastEditionUrl="https://fallstack-22-23.nei-isep.org/"
+            lastEditionUrl="https://fallstack2022.nei-isep.org/"
             neiLogoSrc={{
               white: NeiLogoWhite,
               dark: NeiLogoBlack,
