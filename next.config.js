e199301349a3f5018d2d609114fe92f44ee1f463
Ricardo M <rmoreiradev@gmail.com>
2023-11-14 15:50:45 +0000

    chore: updated nextjs remote pattern cfg for images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "storage.googleapis.com",
+        hostname: "*.storage.googleapis.com",
       },
     ],
   },
