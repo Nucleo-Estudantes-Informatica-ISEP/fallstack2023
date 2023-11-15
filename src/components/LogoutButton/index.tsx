f73fa29642f910d7e1f9c24dc5ed8eee48eb3b7d
Ricardo M <rmoreiradev@gmail.com>
2023-11-15 12:32:34 +0000

    chore: updated logout btn

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -3,13 +3,19 @@
 import { BiLogOut } from "react-icons/bi";
 
 import useSession from "@/hooks/useSession";
+import { BASE_URL } from "@/services/api";
 
 const LogoutButton: React.FC = () => {
   const session = useSession();
 
+  const handleClick = async () => {
+    const res = await fetch(BASE_URL + "/auth/logout", { method: "POST" });
+    if (res.status === 200) session.clear();
+  };
+
   return (
     <button
-      onClick={session.clear}
+      onClick={handleClick}
       className="flex h-full w-full items-center justify-center fill-black text-xl transition-colors hover:text-primary dark:fill-white"
     >
       <BiLogOut />
