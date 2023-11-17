a2b9f552e77f4881a80145859d094dfae468991f
Rui Silva <97447789+rpedros02@users.noreply.github.com>
2023-11-17 13:21:58 +0000

    fix: fixed const name and text size

--- src/components/Faq/index.tsx
+++ src/components/Faq/index.tsx
@@ -3,12 +3,12 @@ export interface FaqProps {
     answer: string;
 }
 
-const Sponsor: React.FC<FaqProps> = ({ question, answer }) => {
+const Faq: React.FC<FaqProps> = ({ question, answer }) => {
     return (
         <div className="flex flex-col justify-center">
             <div className="my-4">
                 <p
-                    className="font-bold text-3xl text-orange-600 text-center "
+                    className="font-bold px-2 pt-5 text-4xl text-orange-600 lg:text-left text-center"
                 >
                     {question}
 
@@ -16,7 +16,7 @@ const Sponsor: React.FC<FaqProps> = ({ question, answer }) => {
             </div>
             <div className="mb-4">
                 <p
-                    className="font-light text-xl px-2 text-justify"
+                    className="font-light lg:text-left text-2xl px-2 text-center"
                 >
                     {answer}
 
@@ -26,4 +26,4 @@ const Sponsor: React.FC<FaqProps> = ({ question, answer }) => {
     );
 };
 
-export default Sponsor;
+export default Faq;
