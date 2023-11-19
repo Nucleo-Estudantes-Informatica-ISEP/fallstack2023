1edc400aab17f0189b726991209c88a6ea2ca567
André L Reis <gato5andre26@gmail.com>
2023-11-19 17:17:51 +0000

    feat: implement company history POST route

---
+++ src/app/api/companies/history/route.tsx
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import prisma from "@/lib/prisma";
+import getServerSession from "@/services/getServerSession";
+import { historySchema } from "@/schemas/historySchema";
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+
+  if (!session)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+  if (session.role !== "COMPANY")
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+
+  const body = await req.json();
+
+  const safeParse = historySchema.safeParse(body);
+  if (!safeParse.success)
+    return NextResponse.json({ message: safeParse.error });
+
+  const { studentCode } = body;
+
+  // check if student exists
+  const student = await prisma.student.findUnique({
+    where: { code: studentCode },
+    include: {
+      user: true,
+    },
+  });
+
+  if (!student)
+    return NextResponse.json({ error: "Student not found" }, { status: 404 });
+
+  // check if student is already scanned
+  const history = await prisma.savedStudent.findFirst({
+    where: { studentId: studentCode, companyId: session.id },
+  });
+
+  if (history)
+    return NextResponse.json(
+      { error: "Student already scanned" },
+      { status: 200 }
+    );
+
+  // create history
+  const entry = await prisma.savedStudent.create({
+    data: {
+      studentId: studentCode,
+      companyId: session.id,
+    },
+  });
+
+  if (!entry)
+    return NextResponse.json(
+      { error: "Error creating history" },
+      { status: 500 }
+    );
+
+  return NextResponse.json({ message: "Student scanned" }, { status: 201 });
+}
