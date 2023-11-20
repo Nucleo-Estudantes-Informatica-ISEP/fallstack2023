00856cdde3731ce9dd51f3c74f03cb6694ab4d31
André L Reis <gato5andre26@gmail.com>
2023-11-20 20:39:11 +0000

    enhancement: added a lil animation to when the user copies it's code

--- src/components/QRCodeTab/PerfilTab/index.tsx
+++ src/components/QRCodeTab/PerfilTab/index.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import React from "react";
+import { motion, useAnimation } from "framer-motion";
 import QRCode from "qrcode.react";
+
 import { UserWithProfile } from "@/types/UserWithProfile";
+
 import { BsFillClipboardFill } from "react-icons/bs";
 
 interface PerfilTabProps {
-    user: UserWithProfile;
+  user: UserWithProfile;
 }
 
-const PerfilTab: React.FC<PerfilTabProps> = ({user}) => {
+const PerfilTab: React.FC<PerfilTabProps> = ({ user }) => {
   // student url
   const baseUrl = window.location.origin;
 
@@ -18,38 +21,67 @@ const PerfilTab: React.FC<PerfilTabProps> = ({user}) => {
       ? `${baseUrl}/student/${user.student.code}`
       : "";
 
+  const controls = useAnimation();
+  const [isCopied, setIsCopied] = React.useState<boolean>(false);
+
   const handleCopyClick = () => {
     if (user.student?.code) {
       navigator.clipboard.writeText(user.student?.code).catch(() => {});
+
+      // start animation when code is copied
+      controls.start({
+        scale: [1, 1.1, 1], // Example animation (scale up and down)
+      });
+
+      // show "Copied!" text
+      setIsCopied(true);
+
+      // reset the "Copied!" text after a short delay
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1500);
     }
   };
 
   return (
     <div className="mt-10 grid grid-cols-1 sm:mt-0 sm:grid-cols-1 md:mt-6 md:grid-cols-2 lg:mt-20 xl:mt-44">
       {/* left column */}
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center">
         <QRCode size={150} value={profileUrl} />
       </div>
       {/* right column */}
       <div className="flex items-center justify-center pb-0 pt-14 sm:pb-0 sm:pt-0">
-        <div className="flex flex-col items-center">
-          <div
+        <div className="flex flex-col items-center relative">
+          <motion.div
             className="rounded-lg bg-gray-200 p-2 sm:p-2 md:p-2"
             onClick={handleCopyClick}
+            whileTap={{ scale: 0.9 }} // Optional: Add a tap effect
+            animate={controls}
+            initial={{ scale: 1 }}
+            style={{ userSelect: "none" }} // Disable text selection
           >
             <div className="flex items-center">
-              <div className="w-56 cursor-pointer border-none bg-gray-200 p-2 text-center text-xl font-bold text-black focus:outline-none sm:w-56 sm:text-3xl md:w-72">
+              <motion.div
+                className="w-56 cursor-pointer border-none bg-gray-200 p-2 text-center text-xl font-bold text-black focus:outline-none sm:w-56 sm:text-3xl md:w-72"
+              >
                 {user.student?.code}
-              </div>
-              <BsFillClipboardFill
-                style={{
-                  fontSize: "1.5rem",
-                  color: "#718096",
-                }}
-                className="cursor-pointer"
-              />
+              </motion.div>
+              <motion.div whileTap={{ scale: 0.9 }}>
+                <BsFillClipboardFill
+                  style={{
+                    fontSize: "1.5rem",
+                    color: "#718096",
+                  }}
+                  className="cursor-pointer"
+                />
+              </motion.div>
             </div>
-          </div>
+          </motion.div>
+          {isCopied && (
+            <p className="text-green-500 mt-2 font-bold">
+              Copiado!
+            </p>
+          )}
           <p className="lg-mr-0 mt-6 text-sm text-black sm:mr-6 md:mr-4 md:text-sm lg:text-base">
             Partilha o teu <b>código</b> com as empresas de forma a poderem{" "}
             <b>guardar</b> o teu perfil!
--- src/components/QRCodeTab/index.tsx
+++ src/components/QRCodeTab/index.tsx
@@ -1,4 +1,4 @@
-// Tab.tsx
+"use client";
 
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
