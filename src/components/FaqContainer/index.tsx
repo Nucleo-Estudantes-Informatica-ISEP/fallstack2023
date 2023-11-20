ad36c70ae5754a35e39a6ec8b05867134574e3e1
Tomás Lopes <1211289@isep.ipp.pt>
2023-11-20 08:49:27 +0000

    fix: fixed faq container width in mobile

--- src/components/FaqContainer/index.tsx
+++ src/components/FaqContainer/index.tsx
@@ -7,7 +7,7 @@ interface FaqContainerProps {
 }
 const FaqContainer: React.FC<FaqContainerProps> = ({ faqs }) => {
   return (
-    <div className="mx-auto flex w-5/6 flex-col">
+    <div className="mx-auto flex w-[90%] flex-col md:w-5/6">
       {faqs.map(({ question, answer }, i) => (
         <Faq key={question} index={i} question={question} answer={answer} />
       ))}
