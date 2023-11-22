839ef45a2ddd7b417dd9c654b6b4441165b9edbc
André Barros <1211299@isep.ipp.pt>
2023-11-22 15:15:42 +0000

    chore: enchanced silver companies

--- src/components/CompaniesContainer/index.tsx
+++ src/components/CompaniesContainer/index.tsx
@@ -23,32 +23,33 @@ const CompaniesContainer: FunctionComponent<CompaniesContainerProps> = ({
       case "Gold":
         return `border-yellow-400 text-yellow-400 drop-shadow-[0px_0px_4px_#facc15] text-4xl md:text-6xl`;
       case "Silver":
-        return `border-gray-500 drop-shadow-[2px_6px_4px_#c0c0c0] text-gray-300  text-4xl md:text-6xl`;
+        return `border-gray-500 drop-shadow-[2px_6px_2px_#c0c0c0] text-gray-300  text-4xl md:text-6xl`;
       default:
         throw new Error("Tier not found");
     }
   };
 
   return (
-    <section
-      className={`mb-12 w-3/4 rounded-3xl border-2 border-black border-opacity-50 ${
-        theme === "light" ? "bg-gray-800" : "bg-white"
-      } bg-opacity-20 pt-10`}
-    >
+    <div className="flex flex-col items-center justify-center">
       <h2
         className={`${getTierStyling(
           tier
-        )} z-0 mx-2 mb-8 text-center font-semibold lg:mb-4`}
+        )} z-0 mx-2 mb-8 text-center font-poppins font-semibold lg:mb-4`}
       >
         {tier}
       </h2>
-
-      <div className="mt-2 grid grid-cols-1 place-items-center justify-center gap-x-12 gap-y-2 xl:grid-cols-2">
-        {companies.map((company) => {
-          return <Company key={company.name} {...company} />;
-        })}
-      </div>
-    </section>
+      <section
+        className={`mb-12 w-3/4  rounded-3xl border-2 border-black border-opacity-50 ${
+          theme === "light" ? "bg-gray-800" : "bg-white"
+        } bg-opacity-20 pt-10`}
+      >
+        <div className="mt-2 grid grid-cols-1 items-center justify-center gap-x-12 gap-y-2 xl:grid-cols-2">
+          {companies.map((company) => {
+            return <Company key={company.name} {...company} />;
+          })}
+        </div>
+      </section>
+    </div>
   );
 };
 
--- src/components/Company/index.tsx
+++ src/components/Company/index.tsx
@@ -16,17 +16,13 @@ export interface CompanyProps {
 
 const Company: React.FC<CompanyProps> = ({ logoHref, name, websiteUrl }) => {
   return (
-    <div className="flex min-h-[8rem] items-center justify-center transition duration-300 ease-in-out hover:scale-105 lg:min-h-[11rem]">
+    <div className="flex h-3/4 min-h-[8rem] w-3/4 items-center justify-center transition duration-300 ease-in-out hover:scale-105 lg:min-h-[11rem]">
       <Link
         rel="noreferrer"
         href={websiteUrl ? websiteUrl : "/company/" + name}
         target="_blank"
       >
-        <Image
-          className="h-full max-h-36 w-full max-w-[10rem] object-cover lg:max-h-28 lg:max-w-[10rem]"
-          src={logoHref}
-          alt={name}
-        />
+        <Image className="object-cover" src={logoHref} alt={name} />
       </Link>
     </div>
   );
