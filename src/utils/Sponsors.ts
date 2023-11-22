c074755da8d44f63e457682b76561abec61a05f4
André Barros <1211299@isep.ipp.pt>
2023-11-22 16:20:29 +0000

    fix: deploy

--- src/utils/Sponsors.ts
+++ src/utils/Sponsors.ts
@@ -1,15 +1,5 @@
 import { SponsorProps } from "../components/Sponsor";
-import { aeIsepLogo, doceAltoLogo } from "./CompaniesImages";
 
 export const Sponsors: SponsorProps[] = [
-  {
-    name: "aeISEP logo",
-    logoHref: aeIsepLogo,
-    website: "https://aeisep.pt/",
-  },
-  {
-    name: "Doce Alto logo",
-    logoHref: doceAltoLogo,
-    website: "https://docealto.pt/",
-  },
+
 ];
