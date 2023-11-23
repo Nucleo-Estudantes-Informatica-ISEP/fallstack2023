c695ee68b75cd9267cc45ac9c235a1b0c2af27af
Samara Miranda <117388696+samaramirandda@users.noreply.github.com>
2023-11-23 17:36:59 +0000

    Update Sponsors.ts

--- src/utils/Sponsors.ts
+++ src/utils/Sponsors.ts
@@ -1,5 +1,30 @@
 import { SponsorProps } from "../components/Sponsor";
+import { artipolLogo, divinalLogo, mouraLogo, teleppizzaLogo, vitalisLogo } from './CompaniesImages';
 
 export const Sponsors: SponsorProps[] = [
-
+   {
+        name: 'Artipol Logo',
+        logoHref: artipolLogo,
+        website: 'https://www.facebook.com/Artipol.net/'
+    },
+    {
+        name: 'Confeitaria Divinal logo',
+        logoHref: divinalLogo,
+        website: 'https://www.facebook.com/divinal.porto/?locale=pt_PT'
+    },
+    {
+        name: 'Confeitaria Moura Logo',
+        logoHref: mouraLogo,
+        website: 'https://confeitariamoura.pt/'
+    },
+    {
+        name: 'Telepizza',
+        logoHref: telepizzaLogo,
+        website: 'https://www.telepizza.pt/'
+    },
+    {
+        name: 'Vitalis Logo',
+        logoHref: vitalisLogo,
+        website: 'https://www.vitalis.pt/'
+    }
 ];
