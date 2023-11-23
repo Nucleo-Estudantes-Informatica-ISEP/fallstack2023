f7d1bfd83721256d9bfdc0823702e906a0f1f9c3
André Barros <1211299@isep.ipp.pt>
2023-11-23 13:57:51 +0000

    fix: heads up styling

--- src/components/HeadsUp/index.tsx
+++ src/components/HeadsUp/index.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const HeadsUp: React.FC = () => {
   return (
-    <section className="my-8">
+    <section className="my-12">
       <h1 className="text-center text-xl font-bold sm:text-2xl md:text-3xl">
-        Precisas de <span className="text-primary">ajuda</span> ? Contacta-nos!
+        Precisas de <span className="text-primary">ajuda</span>? Contacta-nos!
       </h1>
 
       <p className="my-2 text-center text-lg">
