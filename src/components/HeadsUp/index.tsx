c44f3155d4a49668f1a207f898eef74fefc70b0d
André Barros <1211299@isep.ipp.pt>
2023-11-23 13:52:36 +0000

    chore: changed heads-up info

--- src/components/HeadsUp/index.tsx
+++ src/components/HeadsUp/index.tsx
@@ -1,49 +1,20 @@
-import {useEffect, useState} from "react";
 import Link from "next/link";
-import useSession from "../../hooks/useSession";
 
-function HeadsUp() {
-    const [isLogged, setIsLogged] = useState(false);
-    const session = useSession();
+const HeadsUp: React.FC = () => {
+  return (
+    <section className="my-8">
+      <h1 className="text-center text-xl font-bold sm:text-2xl md:text-3xl">
+        Precisas de <span className="text-primary">ajuda</span> ? Contacta-nos!
+      </h1>
 
-    useEffect(() => {
-        if(session.user) {
-            setIsLogged(true);
-        }
-    }, []);
-
-    return (
-        <>
-            {!isLogged && (
-                <div>
-                    <Link href='/signup'>
-                        <div
-                            className='
-                            text-2xl sm:text-3xl md:text-5xl lg:text-5xl
-                            font-bold text-center
-                            hover:drop-shadow-2xl hover:scale-105
-                            transition-all
-                            '
-                        >
-                            Ainda não tens conta? <span className='text-orange-600'>Cria uma</span>
-                        </div>
-                    </Link>
-                    <div
-                        className='
-                        flex flex-col justify-center items-center
-                        text-sm sm:text-sm md:text-lg lg:text-xl
-                        mt-4 mb-7 text-center
-                        max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl
-                        mx-auto
-                        '
-                    >
-                        <p>Ao criares uma conta podes usufruir do evento na sua totalidade.</p>
-                        <p>Criamos um sistema no qual as empresas podem salvar o teu perfil para consultarem mais tarde e gravarem o teu CV para que a tua presenção não seja esquecida!</p>
-                    </div>
-                </div>
-            )}
-        </>
-    );
-}
+      <p className="my-2 text-center text-lg">
+        Envia-nos um email para{" "}
+        <Link href="mailto:support@nei-isep.org" className="text-primary">
+          support@nei-isep.org
+        </Link>
+      </p>
+    </section>
+  );
+};
 
 export default HeadsUp;
