dd0fcebebe9457e93d0321876e4d1e19fe190265
André Barros <1211299@isep.ipp.pt>
2023-11-23 14:07:52 +0000

    feat: added heads up

--- src/components/CompaniesSection/index.tsx
+++ src/components/CompaniesSection/index.tsx
@@ -1,7 +1,5 @@
 import { FunctionComponent } from "react";
 
-import { GoldCompanies } from "@/utils/GoldCompanies";
-
 import { SilverCompanies } from "../../utils/SilverCompanies";
 import CompaniesContainer from "../CompaniesContainer";
 import HeadingText from "../HeadingText";
@@ -10,7 +8,6 @@ const CompaniesSection: FunctionComponent = () => {
   return (
     <section className="flex flex-col items-center text-center">
       <HeadingText text="Empresas" />
-      <CompaniesContainer companies={GoldCompanies} tier="Gold" />
       <CompaniesContainer companies={SilverCompanies} tier="Silver" />
     </section>
   );
