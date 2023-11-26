ddd3a88d94a2be8c75c418285301247bc5fa19b8
André Barros <1211299@isep.ipp.pt>
2023-11-26 13:58:26 +0000

    fix: sponsors section styling and logos

--- src/utils/Sponsors.ts
+++ src/utils/Sponsors.ts
@@ -1,30 +1,42 @@
 import { SponsorProps } from "../components/Sponsor";
-import { artipolLogo, divinalLogo, mouraLogo, teleppizzaLogo, vitalisLogo } from './CompaniesImages';
+import {
+  aeisepLogo,
+  artipolLogo,
+  divinalLogo,
+  mouraLogo,
+  telepizzaLogo,
+  vitalisLogo,
+} from "./CompaniesImages";
 
 export const Sponsors: SponsorProps[] = [
-   {
-        name: 'Artipol Logo',
-        logoHref: artipolLogo,
-        website: 'https://www.facebook.com/Artipol.net/'
-    },
-    {
-        name: 'Confeitaria Divinal logo',
-        logoHref: divinalLogo,
-        website: 'https://www.facebook.com/divinal.porto/?locale=pt_PT'
-    },
-    {
-        name: 'Confeitaria Moura Logo',
-        logoHref: mouraLogo,
-        website: 'https://confeitariamoura.pt/'
-    },
-    {
-        name: 'Telepizza',
-        logoHref: telepizzaLogo,
-        website: 'https://www.telepizza.pt/'
-    },
-    {
-        name: 'Vitalis Logo',
-        logoHref: vitalisLogo,
-        website: 'https://www.vitalis.pt/'
-    }
+  {
+    name: "Artipol Logo",
+    logoHref: artipolLogo,
+    website: "https://www.facebook.com/Artipol.net/",
+  },
+  {
+    name: "Confeitaria Divinal logo",
+    logoHref: divinalLogo,
+    website: "https://www.facebook.com/divinal.porto/?locale=pt_PT",
+  },
+  {
+    name: "Confeitaria Moura Logo",
+    logoHref: mouraLogo,
+    website: "https://confeitariamoura.pt/",
+  },
+  {
+    name: "Telepizza",
+    logoHref: telepizzaLogo,
+    website: "https://www.telepizza.pt/",
+  },
+  {
+    name: "Vitalis Logo",
+    logoHref: vitalisLogo,
+    website: "https://www.vitalis.pt/",
+  },
+  {
+    name: "AEISEP Logo",
+    logoHref: aeisepLogo,
+    website: "https://www.aeisep.pt/",
+  },
 ];
